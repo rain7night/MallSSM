a9054f599f6f8c5294d75651ac23173e726e30d0
rain7night <rainnight@outlook.com>
2017-04-09 23:14:52 +0800

    update tb_item

--- db_data.sql
+++ db_data.sql
@@ -1,5 +1,11 @@
 
-INSERT INTO tb_admin_user VALUES(1,'admin','0cc175b9c0f1b6a831c399e269772661','1243565','11@qq.com','2016-11-1','2016-11-7');
+INSERT INTO tb_admin_user VALUES
+(1,'admin','0cc175b9c0f1b6a831c399e269772661','1243565','11@qq.com','2016-11-1','2016-11-7');
+
+insert into tb_item
+(id, title, sell_point, price, num, barcode, image, cid, status, created, updated)
+values
+(1,'123t','ppp',110,10,'bell',null,11,1,sysdate(),sysdate());
 
 
 
--- manager/manager.web/src/test/java/FTPTest.java
+++ manager/manager.web/src/test/java/FTPTest.java
@@ -7,21 +7,21 @@ import org.junit.Test;
 
 public class FTPTest {
 	
-//	@Test
-//	public void test1() throws Exception{
-//		//创建一个FtpClient对象
-//		FTPClient  ftpClient=new FTPClient();
-//		//创建FTP连接
-//		ftpClient.connect("192.168.186.128",21);
-//		ftpClient.login("root", "handsome");
-//
-//		FileInputStream inputStream=new FileInputStream(new File("D:\\Book\\psb.jpg"));
-//		ftpClient.changeWorkingDirectory("/home/ftpuser/www/images");
-//		ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-//		ftpClient.storeFile("hello2.jpg", inputStream);
-//		//关闭连接
-//		ftpClient.logout();
-//	}
+	@Test
+	public void test1() throws Exception{
+		//创建一个FtpClient对象
+		FTPClient  ftpClient=new FTPClient();
+		//创建FTP连接
+		ftpClient.connect("192.168.186.128",21);
+		ftpClient.login("root", "handsome");
+
+		FileInputStream inputStream=new FileInputStream(new File("D:\\Book\\psb.jpg"));
+		ftpClient.changeWorkingDirectory("/home/ftpuser/www/images");
+		ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+		ftpClient.storeFile("hello2.jpg", inputStream);
+		//关闭连接
+		ftpClient.logout();
+	}
 	
 	
 	
